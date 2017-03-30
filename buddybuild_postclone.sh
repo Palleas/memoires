d5b73288cfa6d6ff6bed8fedabc2adbe043e3ef1
Romain Pouclet <romain.pouclet@gmail.com>
2017-03-30 11:30:41 -0400

    Don't fail with AWS config

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -5,8 +5,8 @@ brew install blender/homebrew-tap/rome
 
 echo "Setting up AWS credentials"
 mkdir -p "${HOME}/.aws" || true
-cp "${BUDDYBUILD_SECURE_FILES}/aws-config" "${HOME}/.aws/config"
-cp "${BUDDYBUILD_SECURE_FILES}/aws-credentials" "${HOME}/.aws/credentials"
+cp "${BUDDYBUILD_SECURE_FILES}/aws-config" "${HOME}/.aws/config" || true
+cp "${BUDDYBUILD_SECURE_FILES}/aws-credentials" "${HOME}/.aws/credentials" || true
 
 echo "Downloading dependencies from S3 using Rome..."
 make deps
