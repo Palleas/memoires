59d78582cd2fe718d4f9dd82e0f933d1cde545df
Romain Pouclet <romain.pouclet@gmail.com>
2017-04-01 17:56:34 -0400

    Silent brew update

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 echo "Installing Rome..."
-make dependencies 
+make dependencies > /dev/null 2>&1
 
 echo "Setting up AWS credentials"
 mkdir -p "${HOME}/.aws"
 cp "${BUDDYBUILD_SECURE_FILES}/aws-config" "${HOME}/.aws/config"
 cp "${BUDDYBUILD_SECURE_FILES}/aws-credentials" "${HOME}/.aws/credentials"
-
-rome list --platform ios
 
 echo "Downloading dependencies from S3 using Rome..."
 make bootstrap
