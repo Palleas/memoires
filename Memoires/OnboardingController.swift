22519546ffc441d1b0316baf77e456ed23410efe
Romain Pouclet <romain.pouclet@gmail.com>
2017-04-02 14:31:32 -0400

    Setup initial auth state

--- Memoires/OnboardingController.swift
+++ Memoires/OnboardingController.swift
@@ -1,23 +1,69 @@
 import Foundation
 import ReactiveSwift
-import ReactiveCocoa
 
+protocol StateTokenFactory {
+    associatedtype TokenType: CustomStringConvertible
+    
+    func generate() -> TokenType
+}
 
-final class OnboardingController {
-
+final class OnboardingController<F: StateTokenFactory>{
     struct Credentials {
         let clientId: String
         let clientSecret: String
     }
     
-    private let credentials: Credentials
+    enum OnboardingError: Error {
+        
+    }
 
-    init(credentials: Credentials) {
-        self.credentials = credentials
+    enum State: Equatable {
+        case openAuthorizeURL(URL)
+        
+        static func ==(lhs: State, rhs: State) -> Bool {
+            switch (lhs, rhs) {
+            case let (.openAuthorizeURL(url1), .openAuthorizeURL(url2)):
+                return url1 == url2
+            default:
+                return false
+            }
+        }
     }
     
-    static func finalizeAuthentication(with url: URL) {
+    private let credentials: Credentials
+    private let redirectURI: String
+    private let tokenFactory: F
+    
+    init(credentials: Credentials, redirectURI: String, tokenFactory: F) {
+        self.credentials = credentials
+        self.redirectURI = redirectURI
+        self.tokenFactory = tokenFactory
+    }
+    
+    func onboard() -> SignalProducer<State, OnboardingError> {
+        let stateToken = tokenFactory.generate()
+        let authURL = self.url(state: stateToken)
+
+        return SignalProducer(value: .openAuthorizeURL(authURL))
+    }
+    
+    func finalizeAuthentication(with url: URL) {
         
     }
     
+    func url(state: F.TokenType) -> URL {
+        let args: [(String, String)] = [
+            ("client_id", credentials.clientId),
+            ("redirect_uri", redirectURI),
+            ("scope", "user repo"),
+            ("state", state.description),
+            ("allow_signup", "false"),
+        ]
+        
+        var comps = URLComponents(url: URL(string: "https://github.com/login/oauth/authorize")!, resolvingAgainstBaseURL: true)!
+        comps.queryItems = args.map(URLQueryItem.init)
+        
+        return comps.url!
+
+    }
 }
--- MemoiresTests/OnboardingSpec.swift
+++ MemoiresTests/OnboardingSpec.swift
@@ -5,10 +5,28 @@ import Nimble
 class OnboardingSpec: QuickSpec {
     override func spec() {
         describe("OnboardingController") {
-            it("builds an URL to redirect the user to") {
-                let credentials = OnboardingController(credentials: .init(clientId: "", clientSecret: ""))
+            var onboarding: OnboardingController<StaticStringTokenFactory>!
+            let factory = StaticStringTokenFactory(wrapped: "state-is-evil")
+            
+            beforeEach {
+                onboarding = OnboardingController(credentials: .init(clientId: "client-id", clientSecret: "client-secret"), redirectURI: "memoires://auth", tokenFactory: factory)
             }
             
+            afterEach {
+                onboarding = nil
+            }
+            
+            it("builds an URL to redirect the user to") {
+                let url = URL(string: "https://github.com/login/oauth/authorize?client_id=client-id&redirect_uri=memoires://auth&scope=user%20repo&state=state-is-evil&allow_signup=false")!
+                
+                let initialState = onboarding.onboard().first()!.value!
+                expect(initialState) == OnboardingController.State.openAuthorizeURL(url)
+            }
+            
+            it("handles auth redirect") {
+                
+            }
+
             it("rejects invalid completion URL") {
                 
             }
@@ -17,5 +35,17 @@ class OnboardingSpec: QuickSpec {
                 
             }
         }
+    }
+}
+
+final class StaticStringTokenFactory: StateTokenFactory {
+    let wrapped: String
+    
+    init(wrapped: String) {
+        self.wrapped = wrapped
+    }
+    
+    func generate() -> String {
+        return wrapped
     }
 }
