2b6da023ddaedb0a1f4a22f7d80bceb3e3b8c673
Romain Pouclet <romain.pouclet@gmail.com>
2017-04-02 11:45:10 -0400

    WIP

---
+++ MemoiresTests/OnboardingSpec.swift
@@ -0,0 +1,21 @@
+import Quick
+import Nimble
+@testable import Memoires
+
+class OnboardingSpec: QuickSpec {
+    override func spec() {
+        describe("OnboardingController") {
+            it("builds an URL to redirect the user to") {
+            
+            }
+            
+            it("rejects invalid completion URL") {
+            
+            }
+            
+            it("request a token") {
+                
+            }
+        }
+    }
+}
