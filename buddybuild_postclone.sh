de86517ffa6df047c1a4d860e18ba031e478d922
Romain Pouclet <romain.pouclet@gmail.com>
2017-04-24 10:02:49 -0400

    Remove rome from postclone script

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-echo "Installing Rome..."
-make dependencies > /dev/null 2>&1
-
-echo "Setting up AWS credentials"
-mkdir -p "${HOME}/.aws"
-cp "${BUDDYBUILD_SECURE_FILES}/aws-config" "${HOME}/.aws/config"
-cp "${BUDDYBUILD_SECURE_FILES}/aws-credentials" "${HOME}/.aws/credentials"
-
-echo "Downloading dependencies from S3 using Rome..."
-time make bootstrap rebuild-assets
+make rebuild-assets
